5510193c171e0fe366e16e0998221a77cbf5356f
profmugomes <murilogomes5792@gmail.com>
2026-09-18 14:49:56 -0300

    Atualizado o items

--- libs/security/items.php
+++ libs/security/items.php
@@ -6,15 +6,7 @@
 namespace MiPhantLibs\security;
 
 class items {
-    public function clean(?string $valor) : string|int|null {
-        if (is_null($valor)) {
-            $txt = '';
-        } else {
-            $txt = trim($valor);
-            $txt = strip_tags($txt);
-            $txt = addslashes($txt);
-        }
-
-        return $txt;
+    public function clean(?string $valor, int $flags = ENT_QUOTES|ENT_SUBSTITUTE, ?string $encoding = 'UTF-8', bool $double_encode = true) : string|int|null {
+        return htmlspecialchars($valor, $flags, $encoding, $double_encode);
     }
 }
